c44d898242af188c96ef04d1235ad099c8086320
TruongXuanTruong <xuantruong28x@gmail.com>
2023-09-05 16:16:44 +0700

    Them file

---
+++ js/cvCaNhan.js
@@ -0,0 +1,24 @@
+function scrollToSection() {
+    const section = document.getElementById('kn');
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+function scrollToSection1() {
+    const section = document.getElementById('sp');
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+function scrollToSection2() {
+    const section = document.getElementById('tTHL');
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+function scrollToSection3() {
+    const section = document.getElementById('cM');
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
